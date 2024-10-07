bd19ec10364443cce0f13f210fe154fcfe5fe2f2
Guillermo Santamarina <guillermo.santamarina-ex@unipar.com>
2024-10-07 19:09:30 -0300

    Segunda parte de agregado

--- LSE.py
+++ LSE.py
@@ -3,3 +3,26 @@ class Node:
         self.data = data
         self.next = None
 
+class SinglyLinkedList:
+    def __init__ (self):
+        self.tail = None
+        self.head = None
+        self.size = 0
+    def append(self, data):
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+
+words = SinglyLinkedList()
+words.append('egg')
+words.append('ham')
+words.append('spam')
+
+current = words.head
+while current:
+    print(current.data)
+    current = current.next
